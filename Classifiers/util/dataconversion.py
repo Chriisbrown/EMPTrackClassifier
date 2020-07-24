78ee5c950aef17a5b361549bfe7dc0e84f315d8d
Christopher Brown <cb719@cb719-laptop.hep.ph.ic.ac.uk>
2020-07-24 10:54:35 +0100

    fixes

--- Classifiers/util/dataconversion.py
+++ Classifiers/util/dataconversion.py
@@ -1,20 +1,31 @@
 import util_funcs
 import DualLinkFormat
 
-events = util_funcs.loadDataSingleFile("hybrid10kv11.root",10,bit=True)
+def loadmodelGBDT():
+    import joblib
+    GBDT = joblib.load("Classifier.pkl")
+    GBDT_parameters = ["LogChi","LogBendChi","LogChirphi", "LogChirz", "trk_nstub",
+                        "layer1","layer2","layer3","layer4","layer5","layer6","disk1","disk2","disk3",
+                        "disk4","disk5","BigInvR","TanL","ModZ","dtot","ltot"]
+
+    return (GBDT,GBDT_parameters)
+
+events = util_funcs.loadDataSingleFile("hybrid10kv11.root",100,bit=True)
 linked_events = []
 
-
+GBDT,GBDT_parameters = loadmodelGBDT()
 
 for i,event in enumerate(events):
 
-
-    #print(event[["LogChi","LogBendChi","LogChirphi","LogChirz", "trk_nstub",
-    #            "layer1","layer2","layer3","layer4","layer5","layer6","disk1","disk2","disk3","disk4","disk5","BigInvR","TanL","ModZ","dtot","ltot"]])
     
-    DualLinkFormat.random_Track()
-    event = DualLinkFormat.assignLinksRandom(event, nlinks=2)
-    linked_events.append(event)
+    sample_event = util_funcs.resample_event(event)
+    print("length of event",len(sample_event))
+    if len(sample_event) > 0:
+        pred = GBDT.predict_proba(sample_event[GBDT_parameters].to_numpy())[:,1]
+        DualLinkFormat.random_Track()
+        sample_event = DualLinkFormat.assignLinksRandom(sample_event, nlinks=2)
+        linked_events.append(sample_event)
+        print(pred)
     
 DualLinkFormat.writepfile("input.txt", linked_events, nlinks=2, emptylinks_valid=True)
 
